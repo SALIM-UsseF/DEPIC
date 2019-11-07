d3e1db1176f57f0fd2b0d8494f3eddb9e30088c6
KylianViaux <55193374+KylianViaux@users.noreply.github.com>
2019-11-07 17:03:09 +0100

    Ajout version basique de l'appli
    Ajout de la version basique de l'appli avec l'affichage des QCU et QCM.

---
+++ src/main/java/com/android/application/applicationmobiledepic/TypeQuestion.kt
@@ -0,0 +1,8 @@
+package com.android.application.applicationmobiledepic
+
+enum class TypeQuestion {
+    QUESTION_CHOIX_MULTIPLE,
+    QUESTION_CHOIX_UNIQUE,
+    QUESTION_REPNSE_LIBRE,
+    QUESTION_A_POINTS
+}
